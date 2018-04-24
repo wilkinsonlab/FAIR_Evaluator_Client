42493e1895d2deb268cb6decaa2aceb0e180d345
Mario_P <marprigod@gmail.com>
2018-04-24 10:32:32 -0400

    Add files via upload

---
+++ FAIRtools/FAIRtools/__init__.py
@@ -0,0 +1,41 @@
+#-*- coding: utf-8 -*-
+#!/usr/bin/env python
+
+
+link = 'http://linkeddata.systems:3000'
+import urllib2
+import json
+
+
+def showup(req):
+    json_off = json.load(req)
+    for i in json_off:
+        for m in i:
+                i[m] = str(i[m])
+                print (m+":  "+i[m])
+        print "\n"
+
+
+def collections():
+    req = urllib2.urlopen(link+'/collections.json')
+    showup(req)
+
+def evaluations():
+    req = urllib2.urlopen(link+'/evaluations.json')
+    showup(req)
+    
+def metrics():
+    req = urllib2.urlopen(link+'/metrics.json')
+    principle = raw_input("Choose your principle: (Ex. R1.1, I3 or all)")
+    if "all" in principle:
+        showup(req)
+    else:
+        json_off = json.load(req)
+        for i in json_off:
+            if principle.upper() == i["principle"]:
+                for m in i:
+                    i[m] = str(i[m])
+                    print (m+":  "+i[m])
+                print "\n"
+
+
---
+++ FAIRtools/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='FAIRtools',
+      version='0.1',
+      description='FAIR Metrics Evaluation Tool',
+      url='https://github.com/wilkinsonlab/FAIR_Evaluator_Client',
+      author='Mario Prieto',
+      author_email='mario.prieto@upm.es',
+      license='Wilkinson Laboratory',
+      packages=['FAIRtools'],
+      zip_safe=False)
